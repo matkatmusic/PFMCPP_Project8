d74d132a0507be27250873da90fb153a1422d2a1
Charles Schiermeyer <matkatmusic@gmail.com>
2021-11-04 13:33:06 -0700

    Improved instructions

--- Vehicle.h
+++ Vehicle.h
@@ -7,6 +7,7 @@ struct Vehicle
 {
     Vehicle(const std::string& n) : name(n) { }
     
+    //Special member Functions.  See instruction 9) and note in main()
     virtual ~Vehicle() = default;
     Vehicle(const Vehicle&) = default;
     Vehicle& operator=(const Vehicle&) = default;
